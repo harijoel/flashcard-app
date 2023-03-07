a88a3a368fbd3b7e0c3f6004ffc60baee6913464
harijoel <harijoel09@gmail.com>
2023-03-07 16:32:34 -0600

    fixed useRef big, modulated Header

--- src/App.js
+++ src/App.js
@@ -1,103 +1,28 @@
 import React, {useState, useEffect, useRef} from "react";
 import FlashcardList from "./FlashcardList";
 import './app.css'
-import axios from "axios";
+
+import Header from "./Header";
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
-  const [categories, setCategories] = useState([])
   
-  const categoryEl = useRef()
-  const ammountEl = useRef()
-
-  useEffect(() => {
-    axios.get('https://opentdb.com/api_category.php')
-    .then(res => {
-      setCategories(res.data.trivia_categories)
-    })
-  }, [])
-
-  useEffect(() => {
-    // axios.get('https://opentdb.com/api.php?amount=10')
-    // .then(res => {
-    //   setFlashcards(res.data.results.map((questionItem, index) => {
-    //     const answer = decodeString(questionItem.correct_answer);
-    //     const options = [
-    //       ...questionItem.incorrect_answers.map(a => decodeString(a)), 
-    //       answer
-    //     ];
-    //     return {
-    //       id: `${index}-${Date.now()}`,
-    //       question: decodeString(questionItem.question),
-    //       answer: answer,
-    //       options: options.sort(() => Math.random() - 0.5)
-    //     }
-    //   })
-    //   );
-
-    // })
-  }, [])
-
-  function decodeString(str) {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML = str
-    return textArea.value
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault()
-    console.log("generate was submited")
-    axios
-    .get('https://opentdb.com/api.php', {
-      params: {
-        amount: ammountEl.current.value,
-        category: categoryEl.current.value
-      }
-    })
-    .then(res => {
-      setFlashcards(res.data.results.map((questionItem, index) => {
-        const answer = decodeString(questionItem.correct_answer);
-        const options = [
-          ...questionItem.incorrect_answers.map(a => decodeString(a)), 
-          answer
-        ];
-        return {
-          id: `${index}-${Date.now()}`,
-          question: decodeString(questionItem.question),
-          answer: answer,
-          options: options.sort(() => Math.random() - 0.5)
-        }
-      })
-      );
-
-    })
-  }
 
   return (
     <>
-      <form className="header" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="category">Category</label>
-          <select id="category" ref={categoryEl}>
-            {categories.map(category => {
-              return <option value={category.id} key={category.id}>{category.name}</option>
-            })}
-          </select>
-        </div>
-        <div className="form-group">
-            <label htmlFor="ammount">Number of Questions</label>
-            <input type="number" id="ammount" min="1" step="1" defaultValue={10} ref={ammountEl} />
-        </div>
-        <div className="form-group">
-            <button className="btn">Generate</button>
-        </div>
-      </form>
+      <Header setFlashcards={setFlashcards} />
       <div className="container">
         <FlashcardList flashcards={flashcards} />
       </div>
     </>
   );
 }
+
+
+
+
+
+
 
 const SAMPLE_FLASHCARDS = [
   {
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import GenerateBtn from './GenerateBtn'
+import InputQuestionNo from './InputQuestionNo'
+import SelectCategory from './SelectCategory'
+import axios from "axios";
 
-export default function Header() {
+export default function Header({setFlashcards}) {
+  const [categories, setCategories] = useState([])
+  const categoryEl = useRef()
+  const ammountEl = useRef()
+
+  useEffect(() => {
+      axios.get('https://opentdb.com/api_category.php')
+      .then(res => {
+        setCategories(res.data.trivia_categories)
+      })
+    }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios
+    .get('https://opentdb.com/api.php', {
+      params: {
+        amount: ammountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer);
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)), 
+          answer
+        ];
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() - 0.5)
+        }
+      })
+      );
+    })
+  }
+  
   return (
-    <div>Header</div>
+    <form className="header" onSubmit={handleSubmit}>
+      <SelectCategory categories={categories} categoryEl={categoryEl} />
+      <InputQuestionNo ammountEl={ammountEl} />
+      <GenerateBtn />
+    </form>
   )
 }
